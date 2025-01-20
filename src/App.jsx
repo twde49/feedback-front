1795800fda2f2266f0538920bfedb333b3d48ef4
Oslynx <malakayalvg@gmail.com>
2025-01-20 13:38:27 +0100

    test malakaya

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
-
+import Kaya from "./pages/kaya.jsx";
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/malakaya" element={<Kaya />} />
       </Routes>
     </div>
   );
---
+++ src/pages/kaya.jsx
@@ -0,0 +1,9 @@
+function Kaya() {
+    return (
+        <>
+        <h2>salut ça va</h2>
+    </>
+    )
+}
+
+export default Kaya
