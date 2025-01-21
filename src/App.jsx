28577828d90c588d12e9e4665e0be752635f0392
sebastien <miantamag@gmail.com>
2025-01-21 13:50:40 +0100

    test merge 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Kaya from "./pages/kaya.jsx";
 import Nuage from "./pages/nuage.jsx";
+import Audio from "./pages/Audio.jsx";
 
 const App = () => {
   return (
@@ -10,6 +11,8 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/malakaya" element={<Kaya />} />
         <Route path="/nuage" element={<Nuage />} />
+        <Route path="/audio" element={<Audio />} />
+
       </Routes>
     </div>
   );
