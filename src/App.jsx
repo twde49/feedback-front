3e8d9d553f05f779a7d25d5093236e2fa083d33d
sebastien <miantamag@gmail.com>
2025-01-21 13:55:31 +0100

    test merge 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home.jsx';
 import Kaya from "./pages/kaya.jsx";
 import Nuage from "./pages/nuage.jsx";
 import Audio from "./pages/Audio.jsx";
+import Pierre from "./pages/Pierre.jsx";
 
 const App = () => {
   return (
@@ -12,6 +13,8 @@ const App = () => {
         <Route path="/malakaya" element={<Kaya />} />
         <Route path="/nuage" element={<Nuage />} />
         <Route path="/audio" element={<Audio />} />
+        <Route path="/pierre" element={<Pierre />} />
+
 
       </Routes>
     </div>
